e45c05e017c02eda5d1e167c3586a2201a796ced
Oliver Atkinson <oliveratkinson@me.com>
2026-05-20 15:43:19 +0100

    Add Umami analytics

--- Sources/Website/MainLayout.swift
+++ Sources/Website/MainLayout.swift
@@ -13,6 +13,11 @@ struct MainLayout: Layout {
         .pageMetadata("theme-color", "#030805")
         .pageResource("/styles.css?v=20260505-background-fade", relationship: .stylesheet)
         .pageResource("/favicon.svg", relationship: .icon)
+        .script("https://cloud.umami.is/script.js") { script in
+            script
+                .defer()
+                .attribute("data-website-id", "aac5108f-59d9-434f-96a1-0dd3b0376b15")
+        }
         .script("/background.js?v=20260505-background-fade") { script in
             script.defer()
         }
