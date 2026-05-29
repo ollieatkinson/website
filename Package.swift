f9307359667a7ea0e82af0f2d7c601017d9cca91
Oliver Atkinson <oliveratkinson@me.com>
2026-05-29 21:42:00 +0100

    Run build checks on branches

--- Package.swift
+++ Package.swift
@@ -19,6 +19,10 @@ let package = Package(
             dependencies: [
                 .product(name: "Raptor", package: "Raptor")
             ]
+        ),
+        .testTarget(
+            name: "WebsiteTests",
+            dependencies: ["Website"]
         )
     ]
 )
